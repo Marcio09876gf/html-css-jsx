f9a048f1c72f3df245de2f9f9f73b42cdd064c6a
Marcio09876gf <gfservicosresidenciais@gmail.com>
2024-06-06 05:18:34 -0300

    modificação no projeto

---
+++ script.js
@@ -0,0 +1,36 @@
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener('click', () =>{
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+function mask(o, f) {
+    setTimeout(function() {
+      var v = mphone(o.value);
+      if (v != o.value) {
+        o.value = v;
+      }
+    }, 1);
+  }
+  
+  function mphone(v) {
+    var r = v.replace(/\D/g, "");
+    r = r.replace(/^0/, "");
+    if (r.length > 10) {
+      r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (r.length > 5) {
+      r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (r.length > 2) {
+      r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+    } else {
+      r = r.replace(/^(\d*)/, "($1");
+    }
+    return r;
+  }
+
+  $(document).ready(function(){
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00.000-000');
+  });
